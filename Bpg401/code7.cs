d9349100fbdb925b594a78f6a57cdbe0e769e2a0
adel5000 <80105965+adel5000@users.noreply.github.com>
2022-09-26 15:49:26 +0300

    Create code7.cs

---
+++ Bpg401/code7.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int y = 10;
+		int r = 100;
+		if (r == 100)
+		if (y == 5)
+				r++;
+		else if (y > 12)
+			r = 0;
+		else
+			r = 50;
+		else
+			r = 50;
+	}
+}
