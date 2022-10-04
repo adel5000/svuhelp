25a9be0168d74c3822a553610ccec43bda8c6dbe
adel5000 <80105965+adel5000@users.noreply.github.com>
2022-10-04 18:31:09 +0300

    Update code1.cs

--- ‏‏Bpg402/code1.cs
+++ ‏‏Bpg402/code1.cs
@@ -1,14 +1,44 @@
 using System;
 
-public class Program
+class Strings
 {
-	public static void Main()
+	static void Main(string[] args)
 	{
-		int i = 0;
-		int y = 8;
-		Console.WriteLine(++i + 8 + y * 5);
-			// 1 + 8 + 8 * 5
-			// 1 + 8 + 40
-			// 49
+		string s1 = "One Two Three Four";
+		int ix;
+		// get the index of the last space
+		ix = s1.LastIndexOf(" ");
+		// get the last word.
+		string s2 = s1.Substring(ix + 1);
+		// set s1 to the substring starting at 0
+		// and ending at ix (the start of the last word
+		// thus s1 has one two three
+		s1 = s1.Substring(0, ix);
+		// find the last space in s1 (after two)
+		ix = s1.LastIndexOf(" ");
+		// set s3 to the substring starting at
+		// ix, the space after "two" plus one more
+		// thus s3 = "three"
+		string s3 = s1.Substring(ix + 1);
+		Console.WriteLine("s2: {0}\ns3: {1}", s2, s3);
+		// reset s1 to the substring starting at 0
+		// and ending at ix, thus the string "one two"
+		s1 = s1.Substring(0, ix);
+		// reset ix to the space between "one" and "two"
+		ix = s1.LastIndexOf(" ");
+		// set s4 to the substring starting one space after ix,
+		// thus the substring "two"
+		string s4 = s1.Substring(ix + 1);
+		// reset s1 to the substring starting at 0 and ending at ix,
+		// thus "one"
+		s1 = s1.Substring(0, ix);
+		// set ix to the last space, but there is none so ix now = -1
+		ix = s1.LastIndexOf(" ");
+		// set s5 to the substring at one past the last space.
+		// there was no last space
+		// so this sets s5 to the substring starting at zero
+		string s5 = s1.Substring(ix + 1);
+		Console.WriteLine("s4: {0}\ns5: {1}\n", s4, s5);
+		Console.WriteLine("s1: {0}\n", s1);
 	}
 }
