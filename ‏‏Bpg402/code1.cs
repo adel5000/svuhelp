b824393aae2448ff7b4c77c5b6d400ef3d1b48a9
adel5000 <80105965+adel5000@users.noreply.github.com>
2022-10-04 17:57:05 +0300

    Add files via upload

---
+++ ‏‏Bpg402/code1.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int i = 0;
+		int y = 8;
+		Console.WriteLine(++i + 8 + y * 5);
+			// 1 + 8 + 8 * 5
+			// 1 + 8 + 40
+			// 49
+	}
+}
